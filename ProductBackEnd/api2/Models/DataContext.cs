d4f668c5f2481dc25833e7979a3834a7bf00b227
Valeria Tapia Fusco <valeria.tapia.fusco@gmail.com>
2019-03-23 11:26:44 -0400

    Api + azure(falta)

---
+++ ProductBackEnd/api2/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace api2.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaulConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<api2.Models.Product> Products { get; set; }
+    }
+}
---
+++ ProductBackEnd/api2/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿
+namespace api2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del Producto")]
+        public string Nombre { get; set; }
+
+        public double Precio { get; set; }
+    }
+}
